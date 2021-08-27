b12177725d7dfd188d08979d28d7d28da20a888b
JafetCaleb <72477875+JafetCaleb@users.noreply.github.com>
2021-08-26 19:28:11 -0500

    nuevoinivio

---
+++ Tutorias001/PruebaUnitarias/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebaUnitarias")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("PruebaUnitarias")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ac63fa9-3b23-432e-9c05-58013568c5a3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tutorias001/PruebaUnitarias/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CapaPresentacion;
+
+namespace PruebaUnitarias
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestLogin()
+        {
+            Login aux = new Login();
+            bool prueba = aux.logins("Cata","cata");
+            Assert.AreEqual(prueba, true);
+        }
+    }
+}
